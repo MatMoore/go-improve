dc46a9192fcd59e9d8f0ec67801a45011958b60d
Mat Moore <matmoore@users.noreply.github.com>
2016-04-15 22:48:30 +0100

    Show the users own games
    These are the games the user created or the user agreed
    to review.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,16 @@ class WelcomeController < ApplicationController
   def index
       # pagination schmagination
       @all = Game.all.includes(:reviews).order(created_at: :desc)
+
+      if user_signed_in?
+          @mine = Game.where(
+              "exists(select 1 from reviews where user_id=? and game_id=games.id) or games.user_id=?",
+              current_user.id,
+              current_user.id,
+          ).order(created_at: :desc)
+      else
+          @mine = Game.none
+      end
   end
 
   def review
